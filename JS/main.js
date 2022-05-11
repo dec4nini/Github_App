afdb61ff995c07de426c27dba2d2817eb65b9031
Eduardo Decanini <eduardodecanini@gmail.com>
2022-05-10 21:24:37 -0300

    Criada a tela de usuários

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+const user_login = user_login
+function change_social_media_links() {
+  for (let li of social_links.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${links_social_media[social]}`
+  }
+}
+
+change_social_media_links()
+
+function get_GitHub_Profil_Info() {
+  const url = `https://api.github.com/users/${links_social_media.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      user_name.textContent = data.name
+      user_bio.textContent = data.bio
+      user_link.href = data.html_url
+      user_avatar.src = data.avatar_url
+      user_login.textContent = data.login
+    })
+}
+
+get_GitHub_Profil_Info()
